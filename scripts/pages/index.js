a8289fb18fe7c1d6f5b09c5431424bf01b402702
Hussonaxel <aaaxel.hsson@gmail.com>
2023-11-23 22:38:51 +0100

    push commit

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,46 +1,60 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
+async function getDataAndCreateElements() {
+  const response = await fetch("./data/photographers.json");
+  const data = await response.json();
+  const photographers = data.photographers;
+  const media = data.media;
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+  const photographerSection = document.querySelector(".photographer_section");
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerTemplate(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    }
+  photographers.forEach((photographer) => {
+    const wrapper = document.createElement("article");
+    wrapper.classList.add("photographer_wrapper");
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    }
-    
-    init();
-    
+    const photographLink = document.createElement("a");
+    photographLink.href = "./photographer.html?id=" + photographer.id;
+    photographLink.classList.add("photographer_link");
+
+    const photographImage = document.createElement("img");
+    photographImage.classList.add("photographer_image");
+    photographImage.src =
+      "./assets/Sample Photos/Photographers ID Photos/" + photographer.portrait;
+    photographImage.alt = photographer.name;
+
+    const photographName = document.createElement("h2");
+    photographName.classList.add("photographer_name");
+    photographName.textContent = photographer.name;
+
+    // Add the image and name to the link
+    photographLink.appendChild(photographImage);
+    photographLink.appendChild(photographName);
+
+    const photographLocation = document.createElement("p");
+    photographLocation.classList.add("photographer_location");
+    photographLocation.textContent =
+      photographer.city + ", " + photographer.country;
+    photographLocation.setAttribute(
+      "aria-label",
+      photographer.city + ", " + photographer.country
+    );
+
+    const photographTagline = document.createElement("p");
+    photographTagline.classList.add("photographer_tagline");
+    photographTagline.textContent = photographer.tagline;
+
+    const photographPrice = document.createElement("p");
+    photographPrice.classList.add("photographer_price");
+    photographPrice.textContent = photographer.price + "€/jour";
+
+    // Add the link to the wrapper
+    wrapper.appendChild(photographLink);
+    wrapper.appendChild(photographLocation);
+    wrapper.appendChild(photographTagline);
+    wrapper.appendChild(photographPrice);
+
+    photographerSection.appendChild(wrapper);
+  });
+
+  return { photographers, media };
+}
+
+getDataAndCreateElements();
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,150 @@
-//Mettre le code JavaScript lié à la page photographer.html
+const urlParams = new URLSearchParams(window.location.search);
+const photographerId = parseInt(urlParams.get("id"));
+
+async function getDataAndCreateElements() {
+  const response = await fetch("./data/photographers.json");
+  const data = await response.json();
+  const photographers = data.photographers;
+  const media = data.media;
+
+  const photographer = photographers.find((p) => p.id === photographerId);
+  photographerModule.createSection(photographer);
+  mediaModule.createSection(media, photographer);
+  mediaModule.createStatsElement(media);
+
+  return { photographers, media };
+}
+
+const photographerModule = {
+  createSection: (photographer) => {
+    console.log("photographer", photographer);
+    const photographerSection = document.querySelector(".photograph-header");
+    const photographerWrapper = document.createElement("div");
+    photographerWrapper.classList.add("photographer_wrapper");
+
+    const photographName = document.createElement("h1");
+    photographName.className = "photographer_name";
+    photographName.textContent = photographer.name;
+
+    const photographLocation = document.createElement("p");
+    photographLocation.className = "photographer_location";
+    photographLocation.textContent = `${photographer.city}, ${photographer.country}`;
+
+    const photographTagline = document.createElement("p");
+    photographTagline.className = "photographer_tagline";
+    photographTagline.textContent = photographer.tagline;
+
+    const photographImage = document.createElement("img");
+    photographImage.className = "photographer_image";
+    photographImage.src = `./assets/Sample Photos/Photographers ID Photos/${photographer.portrait}`;
+    photographImage.alt = photographer.name;
+
+    const photographPrice = document.querySelector(".photographerPrice");
+    photographPrice.textContent = `${photographer.price}€/jour`;
+    photographerWrapper.append(
+      photographName,
+      photographLocation,
+      photographTagline
+    );
+    photographerSection.append(
+      photographImage,
+      document.querySelector(".contact_button"),
+      photographerWrapper
+    );
+  },
+};
+
+const mediaModule = {
+  createSection: (media, photographer) => {
+    const mediaSection = document.querySelector(".media_section");
+    media
+      .filter((item) => item.photographerId === photographer.id)
+      .forEach((item) => {
+        const mediaWrapper = mediaModule.createMediaElement(item, photographer);
+        mediaSection.appendChild(mediaWrapper);
+      });
+  },
+
+  createMediaElement: (mediaItem, photographer) => {
+    const mediaWrapper = document.createElement("article");
+    mediaWrapper.className = "media_wrapper";
+
+    const mediaLink = document.createElement("a");
+    mediaLink.href = `./photographer.html?id=${photographer.id}`;
+    mediaLink.className = "media_link";
+
+    const firstName = photographer.name.split(" ")[0];
+    const mediaElement = mediaItem.image
+      ? mediaModule.createImageElement(mediaItem, firstName)
+      : mediaModule.createVideoElement(mediaItem, firstName);
+
+    const mediaData = document.createElement("div");
+    mediaData.className = "media_data";
+
+    const mediaTitle = document.createElement("h2");
+    mediaTitle.className = "media_title";
+    mediaTitle.textContent = mediaItem.title;
+
+    const mediaLikes = document.createElement("p");
+    mediaLikes.className = "media_likes";
+    mediaLikes.textContent = mediaItem.likes;
+
+    const mediaLikesIcon = document.createElement("img");
+    mediaLikesIcon.src = "./assets/images/red_like.png";
+
+    mediaLikes.appendChild(mediaLikesIcon);
+    mediaData.append(mediaTitle, mediaLikes);
+
+    mediaLink.appendChild(mediaElement);
+    mediaWrapper.append(mediaLink, mediaData);
+
+    return mediaWrapper;
+  },
+
+  createImageElement: (mediaItem, firstName) => {
+    const image = document.createElement("img");
+    image.className = "media_image";
+    image.src = `./assets/Sample Photos/${firstName}/${mediaItem.image}`;
+    image.alt = mediaItem.title;
+    return image;
+  },
+
+  createVideoElement: (mediaItem, firstName) => {
+    const video = document.createElement("video");
+    video.className = "media_image";
+    video.src = `./assets/Sample Photos/${firstName}/${mediaItem.video}`;
+    video.title = mediaItem.title;
+    video.controls = true;
+    return video;
+  },
+
+  createStatsElement: (media) => {
+    const totalLikes = media
+      .filter((item) => item.photographerId === photographerId)
+      .reduce((acc, item) => acc + item.likes, 0);
+      console.log(totalLikes)
+
+      const mediaLikesIcon = document.createElement("img");
+      mediaLikesIcon.className = "total_like_icon";
+      mediaLikesIcon.src = "./assets/images/black_like.png";
+
+
+    const totalLikesElement = document.querySelector(".totalLikes");
+    totalLikesElement.textContent = totalLikes;
+    totalLikesElement.appendChild(mediaLikesIcon);
+
+
+  },
+};
+
+async function sortBy(arg, media) {
+  if (arg === "popularity") {
+    media.sort((a, b) => b.likes - a.likes);
+  } else if (arg === "date") {
+    media.sort((a, b) => new Date(b.date) - new Date(a.date));
+  } else if (arg === "title") {
+    media.sort((a, b) => a.title.localeCompare(b.title));
+  }
+}
+
+getDataAndCreateElements();
