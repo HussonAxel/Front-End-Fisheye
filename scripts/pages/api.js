60aee0b4c103148de196ae5de455fdeaa6e344de
Hussonaxel <aaaxel.hsson@gmail.com>
2023-11-30 09:13:55 +0100

    refactoring api calls to prevent duplicated code

---
+++ scripts/pages/api.js
@@ -0,0 +1,19 @@
+async function fetchData() {
+  try {
+    const response = await fetch("./data/photographers.json");
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    const photographers = data.photographers;
+    const media = data.media;
+
+    // Return data as an object
+    return { data, photographers, media };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return null;
+  }
+}
+
+export default fetchData;
